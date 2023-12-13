6fb04ee1e4440c0f184269e626e37aceaad1de03
Fischer <Florian-Klaus.Fischer@kraussmaffei.com>
2023-12-13 18:25:07 +0100

    Initial 13.12.2023

---
+++ server.js
@@ -0,0 +1,59 @@
+console.log('Starting... please wait!');
+
+const opcua = require("node-opcua");
+
+const certificateManager = new opcua.OPCUACertificateManager({ automaticallyAcceptUnknownCertificate: true, rootFolder: "./certs", });
+
+const config = require('./profiles/simulation/config.json');
+
+
+(async () => {
+    /*** SERVER INIT ***/
+    const server = new opcua.OPCUAServer(
+        {
+            alternateHostname: config.opcua.serverHostname,
+            port: config.opcua.serverPort,
+            resourcePath: config.opcua.resourcePath,
+            buildInfo: {
+                productName: config.opcua.productName,
+                buildNumber: config.opcua.buildNumber,
+                buildDate: new Date(config.opcua.buildDate)
+            },
+            serverCertificateManager: certificateManager
+        }
+    );
+    await server.initialize();
+    console.log("[OK] OPC UA: Server created");
+
+    const addressSpace = server.engine.addressSpace;
+
+    const namespace2 = addressSpace.registerNamespace("KME_Demo_Namespace2");
+    const namespace3 = addressSpace.registerNamespace("KME_Demo_Namespace3");
+
+    const device = namespace2.addObject(
+        {
+            organizedBy: addressSpace.rootFolder.objects,
+            browseName: config.opcua.browseName,
+            nodeId: "ns=2;s=5001",
+        }
+    );
+
+    var serverValues = {}; // Holds all server values
+    module.exports = {
+        namespace3: namespace3,
+        opcua: opcua,
+        serverValues: serverValues,
+    };
+    /*** VARIABLES ***/
+    var myModule = require('./profiles/simulation/variables/Variablen')
+    myModule.run1(addressSpace, device)
+
+    /*** START SERVER ***/
+    await server.start();
+    let endpointUrl = server.endpoints[0].endpointDescriptions()[0].endpointUrl;
+
+    // Replace host name by local ip
+    endpointUrl = endpointUrl.replace(/^opc.tcp:\/\/([^:]+)/, "opc.tcp://127.0.0.1");
+    console.log("[OK] OPC UA: Listening on port " + server.endpoints[0].port);
+    console.log("Endpoint URL: ", endpointUrl);
+})();
